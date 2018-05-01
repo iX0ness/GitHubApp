fad67dd0e23b452da318c809faeaa2ffcf5ce032
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2018-05-01 21:07:14 +0200

    check email

---
+++ GitHubAppTests/RepoDetailVievControllerTest.swift
@@ -0,0 +1,15 @@
+//
+//  RepoDetailVievControllerTest.swift
+//  GitHubAppTests
+//
+//  Created by Levchuk Misha on 01.05.2018.
+//  Copyright © 2018 Levchuk Misha. All rights reserved.
+//
+
+import XCTest
+
+class RepoDetailVievControllerTest: XCTestCase {
+    
+    
+    
+}
